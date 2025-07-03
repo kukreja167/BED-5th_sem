e968c3cd44fc3d3df5a7e2e8ebc99ad7adca4239
kukreja167 <kavyakukreja167@gmail.com>
2025-07-03 15:22:31 +0530

    work

--- lec_1/index1.js
+++ lec_1/index1.js
@@ -2,7 +2,21 @@ const fs=require("fs");
 const path = require("path");
 console.log(fs);
 console.log("starting...");
+setImmediate(() => {
+    console.log("setImmediate");
+});
 setTimeout(() => {
     console.log("settimeout");
-},2000);
-console.log("ending...");
+},0);
+fs.readFile("demo.txt", "utf-8", ( data) => {
+console.log("File read complete");
+setTimeout(() => {
+   console.log("Timeout 2");
+
+},0 );
+setImmediate(() => {
+   console.log("Immediate 2");
+})
+}
+);
+console.log("ending...")
