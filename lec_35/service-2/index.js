b98fd4d79eb42eeff250c7e38bcd5d75234a45ca
kukreja167 <kavyakukreja167@gmail.com>
2025-11-11 21:25:21 +0530

    11_nov_OrderBook

--- lec_35/service-2/index.js
+++ lec_35/service-2/index.js
@@ -1,15 +1,9 @@
 const {createClient} = require('redis');
 let subscriber=createClient();
-
-
-
-
-
 async function notifyMe(){
     await subscriber.connect();
     await subscriber.subscribe('notify-me',(data)=>{
         console.log('data received: ',data);
     });
-
 }
 notifyMe();
--- lec_35/service-3/index.js
+++ lec_35/service-3/index.js
@@ -14,5 +14,6 @@ async function notifyMe(){
         console.log('like received: ',data);
     });
 
+
 }
 notifyMe();
