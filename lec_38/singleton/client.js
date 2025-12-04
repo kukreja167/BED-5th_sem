e0be0603ebce8da9ce66e53a94455b8d0b6eb99d
kukreja167 <kavyakukreja167@gmail.com>
2025-12-04 14:12:21 +0530

    new

--- lec_38/singleton/client.js
+++ lec_38/singleton/client.js
@@ -13,3 +13,13 @@ let principal=principal.getprincipal();
     principal.removesunpension(name);
     
     }
+    console.log("start")
+    async function  ask() {
+        console.log("first");
+await promise.resolve();
+        console.log("lsecond");
+    }
+    ask().then((data)=>{
+        console.log("data");
+    })
+    console.log("third");
